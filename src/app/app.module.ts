16ec749c1acb247155468f3c166eafc7bdfc305f
SheillyR <sheillyrolo@gmail.com>
2021-02-10 18:35:21 -0500

    Created container component - close #3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
+import { ContainerComponent } from './components/container/container.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    ContainerComponent
   ],
   imports: [
     BrowserModule,
