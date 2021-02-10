71f470ebb20e278790a6b6e792c9101485f8c712
SheillyR <sheillyrolo@gmail.com>
2021-02-10 18:36:10 -0500

    Created character component - close #4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
 import { ContainerComponent } from './components/container/container.component';
+import { CharacterComponent } from './components/character/character.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    ContainerComponent
+    ContainerComponent,
+    CharacterComponent
   ],
   imports: [
     BrowserModule,
