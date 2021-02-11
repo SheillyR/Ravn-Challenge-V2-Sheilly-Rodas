19c04fd6e02b771d800f4f8847de8806d784b1d4
SheillyR <sheillyrolo@gmail.com>
2021-02-10 21:18:11 -0500

    Added home route in app-routing.module file and example of apollo angular

--- src/app/components/container/container.component.ts
+++ src/app/components/container/container.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Apollo, gql} from 'apollo-angular';
 
 @Component({
   selector: 'app-container',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./container.component.scss']
 })
 export class ContainerComponent implements OnInit {
+  rates!: any[];
+  loading = true;
+  error: any;
+  constructor(private apollo: Apollo) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.apollo
+      .watchQuery({
+        query: gql`
+          {
+            rates(currency: "USD") {
+              currency
+              rate
+            }
+          }
+        `,
+      })
+      .valueChanges.subscribe((result: any) => {
+        this.rates = result?.data?.rates;
+        this.loading = result.loading;
+        this.error = result.error;
+      });
   }
 
 }
