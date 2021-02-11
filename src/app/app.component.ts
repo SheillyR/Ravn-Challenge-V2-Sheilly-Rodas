c5fd528643d2cd0f7b3a2f6b4917da236a6ac010
SheillyR <sheillyrolo@gmail.com>
2021-02-11 14:36:28 -0500

    Removed call to API from app.component.ts file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,4 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { Apollo } from 'apollo-angular';
-import gql from 'graphql-tag'; 
-/* interface Characters {
-  people: People;
-}
-
-interface People{
-  id: string;
-  name: string;
-}
-
-interface Response{
-  allPeople: Characters[];
-} */
-
-const GET_CHARACTERS = gql`
-query Characters {
-  allPeople{
-    people{
-      id
-      name
-    }
-    
-  }
-}
-`;
 
 @Component({
   selector: 'app-root',
@@ -35,24 +7,8 @@ query Characters {
 })
 export class AppComponent {
   title = 'ravn-challenge-v2';
-  characters!: Observable<any>;
 
-  constructor(private apollo: Apollo) { }
+  constructor() { }
 
-  ngOnInit(): void {
-    this.characters = this.apollo.watchQuery<any>({
-      query: gql`
-      query {
-        allPeople{
-          people{
-          id
-          name
-          }
-          
-        }
-      
-      }
-      `
-    }).valueChanges.pipe(map(result => result.data.allPeople.people));
-  }
+  ngOnInit(): void {}
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag'; 
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  characters!: Observable<any>;
 
-  constructor() { }
+  constructor(private apollo: Apollo) { 
+  }
+
+  getCharacters(): Observable<any> {
+    return this.characters = this.apollo.watchQuery<any>({
+      query: gql`
+      query {
+        allPeople{
+          people{
+          id
+          name
+          }
+          
+        }
+      
+      }
+      `
+    }).valueChanges.pipe(map(result => result.data.allPeople.people));
+  }
 }
